7b7065bbd30058efd86bf291d357a257a21748ec
Carlos Escalante <cescalante2988@gmail.com>
2024-01-30 10:15:53 -0600

    Add vue zero to mastery code examples

---
+++ vue-zero-to-mastery/002 section-22-2-STARTER/src/components/EmailInput.vue
@@ -0,0 +1,29 @@
+<template>
+    <input
+        type="email"
+        class="form-control"
+        placeholder="E-mail"
+        :value="email"
+        @input="update($event)"
+        :class="{
+            'is-valid':
+            email.length >= 4 && email.length > 0,
+            'is-invalid':
+            email.length < 4 && email.length > 0,
+        }"  
+    />
+</template>
+
+<script setup>
+const {email} = defineProps({ 
+    // modelValue: String
+    email: String
+})
+
+const emit = defineEmits(['update:email'])
+
+const update = (event) => {
+    emit('update:email', event.target.value)
+}
+
+</script>
