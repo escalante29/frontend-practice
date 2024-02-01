3daf55d9107683f0646c807cce85a2ce121792d3
Carlos Escalante <cescalante2988@gmail.com>
2024-02-01 15:51:07 -0600

    02. Sum challenge

---
+++ vanilla js and html/advanced-concepts/interview challenges/01-basic-challenges-1/02-get-sum/get-sum.js
@@ -0,0 +1,12 @@
+/**
+ * Returns the sum of two numbers.
+ * @param {number} a - the first number.
+ * @param {number} b - the second number.
+ * @returns {number} - the sum of the two numbers.
+ */
+
+function getSum(a, b) {
+  return a + b;
+}
+
+module.exports = getSum;
