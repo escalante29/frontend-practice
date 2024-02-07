a7dc341a3a8b3c38d1d10fe80163d14895bd9c15
Carlos Escalante <cescalante2988@gmail.com>
2024-02-07 14:20:12 -0600

    55. Balanced parenthesis stack challenge

---
+++ vanilla js and html/advanced-concepts/interview challenges/07-stacks-queues-linked-lists/04-balanced-parenthesis/balanced-parenthesis-run.js
@@ -0,0 +1,6 @@
+const isBalanced = require('./balanced-parenthesis');
+
+const result1 = isBalanced('()()()');
+const result2 = isBalanced('()(');
+
+console.log(result1, result2);
---
+++ vanilla js and html/advanced-concepts/interview challenges/07-stacks-queues-linked-lists/04-balanced-parenthesis/balanced-parenthesis.js
@@ -0,0 +1,23 @@
+const Stack = require('./stack');
+
+/**
+ * Returns true if the parenthesis in a string are balanced.
+ * @param {string} str - The string to check.
+ * @returns {boolean} - Whether the parenthesis in the string are balanced.
+ */
+function isBalanced(str) {
+  const myStack = new Stack();
+  for (char of str) {
+    if (char === '(') {
+      myStack.push(char);
+    } else if (char === ')') {
+      if (myStack.isEmpty()) {
+        return false;
+      }
+      myStack.pop();
+    }
+  }
+  return myStack.isEmpty();
+}
+
+module.exports = isBalanced;
