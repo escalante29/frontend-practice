bc694344cb477e1b29900309e24a29d332dbb0a9
Carlos Escalante <cescalante2988@gmail.com>
2024-02-06 19:06:36 -0600

    Custom stack implementation

---
+++ vanilla js and html/advanced-concepts/interview challenges/07-stacks-queues-linked-lists/02-stack-implementation/stack-run.js
@@ -0,0 +1,12 @@
+const Stack = require('./stack');
+
+const myStack = new Stack();
+
+myStack.push(1);
+myStack.push(2);
+myStack.push(3);
+
+console.log(myStack.pop());
+console.log(myStack.peek());
+console.log(myStack.peek());
+console.log(myStack);
