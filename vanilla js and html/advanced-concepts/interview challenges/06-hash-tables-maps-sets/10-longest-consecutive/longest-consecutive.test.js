145096bf3478fb7e7f7c5dd2c6dad2e6790003eb
Carlos Escalante <cescalante2988@gmail.com>
2024-02-06 18:15:54 -0600

    Fix typo

--- vanilla js and html/advanced-concepts/interview challenges/06-hash-tables-maps-sets/10-longest-consecutive/longest-consecutive.test.js
+++ vanilla js and html/advanced-concepts/interview challenges/06-hash-tables-maps-sets/10-longest-consecutive/longest-consecutive.test.js
@@ -1,4 +1,4 @@
-const longestConsecutiveSequence = require('./longest-consecutive');
+const { longestConsecutiveSequence } = require('./longest-consecutive');
 
 test('Longest Consecutive Sequence', () => {
   expect(longestConsecutiveSequence([100, 4, 200, 1, 3, 2])).toBe(4);
