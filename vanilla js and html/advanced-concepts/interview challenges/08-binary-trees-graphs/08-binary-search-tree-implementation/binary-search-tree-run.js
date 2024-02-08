cda36c734bc01f6419632761453424e00ca2f34f
Carlos Escalante <cescalante2988@gmail.com>
2024-02-08 14:32:09 -0600

    BST Implementation

---
+++ vanilla js and html/advanced-concepts/interview challenges/08-binary-trees-graphs/08-binary-search-tree-implementation/binary-search-tree-run.js
@@ -0,0 +1,19 @@
+const { Node, BinarySearchTree } = require('./binary-search-tree');
+
+// Create a binary search tree:
+//       10
+//      /  \
+//     5    15
+//    /
+//   2
+
+const bst = new BinarySearchTree();
+
+bst.insert(10);
+bst.insert(5);
+bst.insert(15);
+bst.insert(2);
+
+bst.remove(5);
+bst.printTree();
+console.log(bst.lookup(10));
