691ba94feed3094a0250f98adf0a8a9fb112db39
Carlos Escalante <cescalante2988@gmail.com>
2024-02-08 19:09:15 -0600

    Selection sort

---
+++ vanilla js and html/advanced-concepts/interview challenges/09-sorting-algorithms/07-selection-sort-implentation/selection-sort.js
@@ -0,0 +1,23 @@
+/**
+ * Sorts an array using the Selection Sort algorithm.
+ * @param {number[]} array - The array to be sorted.
+ * @returns {number[]} - The sorted array.
+ */
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let minIndex = i;
+
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    if (minIndex !== i) {
+      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+    }
+  }
+  return arr;
+}
+
+module.exports = selectionSort;
